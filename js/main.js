e5714d9b48472b8062af86ebe6637673b884952f
Kris <dauthi66@gmail.com>
2021-11-11 12:55:21 -0800

    set up ToDoItem object and functions to populate.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
 window.onload = function () {
     var picker = datepicker('#due_date');
     picker.setMin(new Date());
+    var addItem = $("add_item");
+    addItem.onclick = process;
 };
+function process() {
+    if (isValid()) {
+        var item = getToDoItem();
+        displayToDoItem(item);
+    }
+}
+var ToDoItem = (function () {
+    function ToDoItem(itemName, dueDate, isComplete) {
+        this.itemName = itemName;
+        this.dueDate = dueDate;
+        this.isComplete = isComplete;
+    }
+    return ToDoItem;
+}());
+function isValid() {
+    return true;
+}
+function getToDoItem() {
+    var itemName = $HTMLinput("item_name").value;
+    var dueDate = new Date($HTMLinput("due_date").value);
+    var isComplete = $HTMLinput("is_complete").checked;
+    var newItem = new ToDoItem(itemName, dueDate, isComplete);
+    return newItem;
+}
+function displayToDoItem(item) {
+}
+function $HTMLinput(id) {
+    return document.getElementById(id);
+}
+function $(id) {
+    return document.getElementById(id);
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,74 @@
 window.onload = function(){
-    const picker = datepicker('#due_date');
-    picker.setMin(new Date());
 
+    //creates date picker function on due date
+    //@ts-ignore: (ignore error) Datepicker does not have intellisense
+    const picker = datepicker('#due_date');
+    picker.setMin(new Date()); //sets min to today.
+
+    let addItem = $("add_item");
+    addItem.onclick = process;
 }
+
+function process(){
+    if (isValid()){
+        let item = getToDoItem();
+        displayToDoItem(item);
+    }
+}
+
+class ToDoItem{
+    itemName:string;
+    dueDate: Date;
+    isComplete:boolean;
+
+    constructor(itemName:string, dueDate:Date, isComplete:boolean){
+        this.itemName = itemName;
+        this.dueDate = dueDate;
+        this.isComplete = isComplete;
+    }
+}
+
+/**
+ * Check form data is valid
+ */
+function isValid():boolean{
+    return true;
+}
+
+/**
+ * get input from form and set int toDoItem
+ */
+function getToDoItem():ToDoItem{
+
+    //set item's title to value given on title input
+    let itemName = $HTMLinput("item_name").value;
+    //set item's due date to a new Date using input selected by user
+    let dueDate = new Date($HTMLinput("due_date").value);
+    //set items complete toggle to true if checked
+    let isComplete = $HTMLinput("is_complete").checked;
+
+    let newItem = new ToDoItem(itemName, dueDate, isComplete);
+    
+    return newItem;
+}
+
+/**
+ * Displays ToDoItem on To Do List
+ */
+function displayToDoItem(item:ToDoItem):void{
+    
+}
+
+//refactor cast and get by id.
+function $HTMLinput(id):HTMLInputElement {
+    return <HTMLInputElement>document.getElementById(id);
+}
+
+//refactor cast and get by id.
+function $(id):HTMLElement {
+    return document.getElementById(id);
+}
+//let item = new ToDoItem("Testing", new Date(2021, 12, 11), false);
+
+//Task: Allow user to mark a ToDoItem as completed
+//Task: Store ToDOItems in web storage.
